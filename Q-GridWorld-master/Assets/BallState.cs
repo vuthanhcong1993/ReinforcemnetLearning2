4cdb2d0362312390a8f1488606b26988aa97ad20
vuthanhcong1996 <vuthanhcong96@gmail.com>
2019-01-20 18:04:19 +0700

    First Commit

---
+++ Q-GridWorld-master/Assets/BallState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallState : MonoBehaviour {
+
+	public bool dropped = false;
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag == "drop")
+		{
+			dropped = true;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
